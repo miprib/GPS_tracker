02f5a00ec73e0a1e56196553dc8b0dfb8e4cf4a4
Mikas <mikas.prib@gmail.com>
2018-07-06 15:00:05 +0300

    Tweaks and Additions
    Added more methods to use the GPS class. Tweaked Manifest.xml and activity_main.xml.

--- GPS/app/src/main/java/gps/app/MainActivity.java
+++ GPS/app/src/main/java/gps/app/MainActivity.java
@@ -2,12 +2,41 @@ package gps.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnShowLocation;
+
+    GPS gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnShowLocation = (Button) findViewById(R.id.show_Location);
+
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gps = new GPS(MainActivity.this);
+
+                if(gps.canGetLocation()) {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "YourLocation is -\nLat: " + latitude +
+                            "\nLong: " + longitude,
+                            Toast.LENGTH_LONG).show();
+                } else{
+                    gps.showSettingsAlert();
+                }
+            }
+        });
     }
 }
